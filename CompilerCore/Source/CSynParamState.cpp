23fe6d8568988dd35823b6a63c9ed2c4ea75b3a0
Victor Hugo Flores Marquez <35241075+lGaze@users.noreply.github.com>
2018-10-12 00:14:43 -0500

    avances en param

--- CompilerCore/Source/CSynParamState.cpp
+++ CompilerCore/Source/CSynParamState.cpp
@@ -17,8 +17,35 @@ void CompilerCore::CSynParamState::checkSyntax()
 	do 
 	{
 		t = m_lex->getNextToken();
+		if (t->getType() != TOKEN_TYPE::ID)
+		{
+			//Error 
+		}
+		else
+		{
+		 
+		}
+		t = m_lex->getNextToken();
+		if (!t->getLex().compare(":") == 0)
+		{
+			//Error
+		}
+		else
+		{
+
+		}
+		if (!t->getLex().compare("int") || !t->getLex().compare("float") || !t->getLex().compare("bool") || !t->getLex().compare("string"))
+		{
+			
+		}
+		else
+		{
+			//Error
+		}
 
 	} while (t->getLex().compare(")") == 0);
+
+	
 }
 
 
